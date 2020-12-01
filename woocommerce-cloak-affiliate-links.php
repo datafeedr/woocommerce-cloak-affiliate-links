30fe1e4d80224d9239b1393ab73d1632f20198e1
Eric <ericbusch@gmail.com>
2020-12-01 11:43:19 -0500

    bumping version

--- woocommerce-cloak-affiliate-links.php
+++ woocommerce-cloak-affiliate-links.php
@@ -7,11 +7,11 @@ Author: datafeedr.com
 Author URI: http://www.datafeedr.com
 License: GPL v3
 Requires at least: 4.7.0
-Tested up to: 5.6-alpha
-Version: 1.0.24
+Tested up to: 5.6
+Version: 1.0.25
 
 WC requires at least: 3.0
-WC tested up to: 4.5
+WC tested up to: 4.7
 
 WooCommerce Cloak Affiliate Links plugin
 Copyright (C) 2020, Datafeedr - help@datafeedr.com
@@ -37,7 +37,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Define constants.
  */
-define( 'WCCAL_VERSION', '1.0.24' );
+define( 'WCCAL_VERSION', '1.0.25' );
 define( 'WCCAL_URL', plugin_dir_url( __FILE__ ) );
 define( 'WCCAL_PATH', plugin_dir_path( __FILE__ ) );
 define( 'WCCAL_BASENAME', plugin_basename( __FILE__ ) );
