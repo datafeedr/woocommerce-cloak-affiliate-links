2e3198b7187b3137a3ce80a545f4e33a74c73090
Eric <ericbusch@gmail.com>
2021-03-08 14:23:36 -0500

    Updated tested up to for WP 5.7

--- woocommerce-cloak-affiliate-links.php
+++ woocommerce-cloak-affiliate-links.php
@@ -7,14 +7,14 @@ Author: datafeedr.com
 Author URI: http://www.datafeedr.com
 License: GPL v3
 Requires at least: 4.7.0
-Tested up to: 5.6
-Version: 1.0.25
+Tested up to: 5.7
+Version: 1.0.26
 
 WC requires at least: 3.0
-WC tested up to: 4.7
+WC tested up to: 5.0
 
 WooCommerce Cloak Affiliate Links plugin
-Copyright (C) 2020, Datafeedr - help@datafeedr.com
+Copyright (C) 2021, Datafeedr - help@datafeedr.com
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -37,7 +37,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Define constants.
  */
-define( 'WCCAL_VERSION', '1.0.25' );
+define( 'WCCAL_VERSION', '1.0.26' );
 define( 'WCCAL_URL', plugin_dir_url( __FILE__ ) );
 define( 'WCCAL_PATH', plugin_dir_path( __FILE__ ) );
 define( 'WCCAL_BASENAME', plugin_basename( __FILE__ ) );
